7027adbceefae55a1908d0054c81fed68374411d
ajmoritz2 <chickenville.trade.head@gmail.com>
2024-03-04 20:35:09 -0500

    Estimated map distance shoot speed! Yay! no test

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -11,6 +11,9 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.Interpolator;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.generated.TunerConstants;
@@ -43,6 +46,8 @@ public class Vision {
         BACK
     }
 
+    private InterpolatingTreeMap<Double, Double> speakerMap = new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Interpolator.forDouble());
+
     private PhotonCamera cameraInUse;
     private PhotonPipelineResult resultsInUse;
     private Transform3d currentCamTrans;
@@ -51,11 +56,18 @@ public class Vision {
     private PhotonCamera camF;
     private PhotonCamera camB;
 
+    // {Distance, Shooter angle} -> Linear InterORIation
+    private double[][] distanceAngles = {{1.74, 0}, {2.69, 12}, {3.69, 28}, {4.65, 32}, {5.65, 38}, {6.70, 40}};
+
     public Vision(){
         camF = new PhotonCamera(Constants.Vision.camLName);
         camB = new PhotonCamera(Constants.Vision.camRName);
         layout =  AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
 
+        for (double[] vals : distanceAngles) {
+            // ULTRAKILL
+            speakerMap.put(vals[0], vals[1]);
+        }
         /* Keeping for might later use
         fPoseEst = new PhotonPoseEstimator(layout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Constants.Vision.frontCameraLocation);
         bPoseEst = new PhotonPoseEstimator(layout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Constants.Vision.backCameraLocation);
@@ -103,9 +115,15 @@ public class Vision {
      * @return the value to shove through the y drive, PID included
      */
     public double driveDistFromTarget(PhotonTrackedTarget target, double targetHeight, double targetPitch, double dMeters) {
+        
         return TunerConstants.drivePID.calculate(PhotonUtils.calculateDistanceToTargetMeters(
             Constants.Vision.frontCameraLocation.getY(), targetHeight, Constants.Vision.frontCameraLocation.getRotation().getY()
             , targetPitch), dMeters);
+    }
+
+    public double shooterSpeedFromDist(PhotonTrackedTarget target){
+        System.out.println("Distance from target X: %s | Distance from target Y: %s".formatted(target.getBestCameraToTarget().getX(), target.getBestCameraToTarget().getY()));
+        return speakerMap.get(Math.sqrt(Math.pow(target.getBestCameraToTarget().getX(),2) + Math.pow(target.getBestCameraToTarget().getY(), 2)));
     }
 
     public PhotonTrackedTarget getFiscalIDTarget(int id, List<PhotonTrackedTarget> visibleTargets) {
