ff1dd787c7dbcfa107e87744ba5aefd5434ee1bd
jashow55 <jason.howerton@gmail.com>
2024-03-06 20:42:37 -0500

    merge with main -small tweak to add smartdashboard import -output vision distance and if it is at trap distance

--- src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import java.util.function.Supplier;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
@@ -17,6 +19,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -94,11 +97,18 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
     }
 
     public void actuallyDrive(SwerveRequest.FieldCentric request,CommandXboxController xboxController) {
-         Vision vision = Robot.m_robotContainer.m_Vision;
-         double stageD = vision.distanceToStage();
-
+        Vision vision = Robot.m_robotContainer.m_Vision;
+        double stageD = vision.distanceToStage();
+        
+        SmartDashboard.putNumber("TrapDistance", stageD);
+        Logger.recordOutput("Vision/Distance",  stageD);
         if (stageD > 1.5 && stageD < 1.7) {
-           
+            Logger.recordOutput("Vision/TrapGood",  true);
+            SmartDashboard.putBoolean("TrapGood", true);
+        }
+        else{           
+            Logger.recordOutput("Vision/TrapGood",  false);
+            SmartDashboard.putBoolean("TrapGood", false);
         }
         /*
         if (xboxController.y().getAsBoolean()) // When Y is pressed Hopefully you will lock onto Fiscal Target 8.
