b7731f4741477549f2393b2a74b89f74d900c98e
jashow55 <jason.howerton@gmail.com>
2024-03-07 19:51:01 -0500

    kettering practice - amp 2, 3, 4, 5 piece paths. -podium angle change 22 to 20 -1M angle change 20 to 18

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -97,8 +97,8 @@ public final class Constants {
         public static final double AMP = 2;
         public static final double TRAP = 0;   //distance 1.57M
         public static final double SPEAKER = 0;
-        public static final double SPEAKER_1M = 20;
-        public static final double SPEAKER_PODIUM = 22; //24
+        public static final double SPEAKER_1M = 18; //20
+        public static final double SPEAKER_PODIUM = 20; //22 //24
         public static final double INTAKE = 12;   //14
         public static final double BUMP_VALUE = .5;    //rotations
         public static final double ATANGLE_TIMEOUT = 1;  //seconds
